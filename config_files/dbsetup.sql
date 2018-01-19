4c398a52912073c01470ad1f598ba2c889b9fb56
Anoop M <noop2016@gmail.com>
2018-01-19 06:32:08 +0000

    slim version. sent config to cloud

--- config_files/dbsetup.sql
+++
@@ -1,5 +0,0 @@
--- create user credentials for flask and database
-CREATE USER IF NOT EXISTS 'flask'@'%' IDENTIFIED BY 'f1ask';
-CREATE DATABASE IF NOT EXISTS url_db;
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON url_db.* TO 'flask'@'%';
-FLUSH PRIVILEGES;
